c27eb397a1f649f8aa2f682e71a2e0f7ea8dba7f
KanishqRaj <727821tuec102@skct.edu.in>
2023-06-05 10:29:14 +0530

    D5 d7

---
+++ Day7/D7/src/test/java/com/selenium/D7/D7t1.java
@@ -0,0 +1,10 @@
+package com.selenium.D7;
+
+
+import org.testng.annotations.Test;
+
+public class D7t1 {
+  @Test
+  public void f() {
+  }
+}
---
+++ Day7/day7/src/test/java/com/selenium/day7/d7t1.java
@@ -0,0 +1,9 @@
+package com.selenium.day7;
+
+import org.testng.annotations.Test;
+
+public class d7t1 {
+  @Test
+  public void f() {
+  }
+}
